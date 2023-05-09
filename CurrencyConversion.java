5cd78ccad0df3373357ded77fd9cd0e45638c19a
Jenna <104884851+jennah1016@users.noreply.github.com>
2023-05-08 23:53:32 -0500

    Add files via upload

--- CurrencyConversion.java
+++ CurrencyConversion.java
@@ -1,49 +1,43 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
-
 public class CurrencyConversion {
-    public static void main (String[] args) {
+    public static void main (String[] args){
         /*this program converts USD to different currencies, and was designed to help me practice the following:
             - if/else statements
-            - basic math operations
+            - basic math operations in Java, math class operations
             - while loops
-         - ask user to select which currency to convert to - choose from CAD, JPY, GBP, EUR
+            - decimal format
+         - ask user to select which currency to convert to - choose from CAD, GBP, EUR, or JPY
          - display monetary value in USD and selected currency
-         - program runs until the user decides to exit*/
+         - program runs until the user decides to exit
+         */
 
         Scanner input = new Scanner(System.in);
         System.out.println("This program can convert USD to CAD, GBP, EUR, or JPY.");
-        System.out.println("Select a which currency you want to convert to...");
+        System.out.println("Select which currency you want to convert to...");
         choose('C', "CAD");
         choose('G', "GBP");
         choose('E', "EUR");
         choose('J', "JPY");
         System.out.println("Or type (X) to exit");
         String in = input.next().toLowerCase();
-            while (!in.equals("x")){
-                if (in.equals("c")) {
-                    cad();
-                }
-                if (in.equals("g")) {
-                    gbp();
-                }
-                if (in.equals("e")) {
-                    eur();
-                }
-
-                if (in.equals("j")) {
-                    jpy();
-                } if (in.equals("x")){
-                    System.out.println("program exited");
-                } else {
-                    System.out.println("Select a which currency you want to convert to...");
-                    choose('C', "CAD");
-                    choose('G', "GBP");
-                    choose('E', "EUR");
-                    choose('J', "JPY");
-                    System.out.println("Or type (X) to exit");
-                    in = input.next().toLowerCase();
-                }
-                System.out.println("Select a which currency you want to convert to...");
+        while (!in.equals("x")){
+            if(in.equals("c")){
+                cad();
+            }
+            if(in.equals("g")){
+                gbp();
+            }
+            if (in.equals("e")){
+                eur();
+            }
+            if (in.equals("j")){
+                jpy();
+            }
+            if (in.equals("x")){
+                System.out.println("program exited");
+            } else {
+                System.out.println("Select which currency you want to convert to...");
                 choose('C', "CAD");
                 choose('G', "GBP");
                 choose('E', "EUR");
@@ -51,11 +45,8 @@ public class CurrencyConversion {
                 System.out.println("Or type (X) to exit");
                 in = input.next().toLowerCase();
             }
-    }
-
-    //I did not want to type the same sentence over again, hence why this method exists
-    public static void choose(char x, String y){
-        System.out.println("Type (" + x + ") to convert to " + y);
+            System.out.println();
+        }
     }
 
     //individual methods for currency conversion
@@ -69,8 +60,10 @@ public class CurrencyConversion {
         }
         //convert USD to given currency
         double jpy = value * 134.74;
-        //display value in USD and given currency
-        System.out.println(value + " USD is worth " + jpy + " JPY.");
+
+        //display value in USD and given currency, round JPY to nearest whole number
+        System.out.println(value + " USD is worth " + Math.round(jpy) + " JPY.");
+        System.out.println();
     }
     public static void eur(){
         System.out.println("Enter a number: ");
@@ -79,8 +72,11 @@ public class CurrencyConversion {
         if (value < 0){
             throw new IllegalArgumentException("Please enter a value greater than or equal to 0.");
         }
+        //round euro, gbp, and cad to 2 decimal places
+        DecimalFormat dec = new DecimalFormat("##.00");
         double eur = value * 0.91;
-        System.out.println(value + " USD is worth " + eur + " EUR.");
+        System.out.println(value + " USD is worth " + dec.format(eur) + " EUR.");
+        System.out.println();
     }
     public static void gbp(){
         System.out.println("Enter a number: ");
@@ -89,17 +85,25 @@ public class CurrencyConversion {
         if (value < 0){
             throw new IllegalArgumentException("Please enter a value greater than or equal to 0.");
         }
+        DecimalFormat dec = new DecimalFormat("##.00");
         double gbp = value * 0.8;
-        System.out.println(value + " USD is worth " + gbp + " GBP.");
+        System.out.println(value + " USD is worth " + dec.format(gbp) + " GBP.");
+        System.out.println();
     }
-    public static void cad(){
+    public static void cad(){ //multiply by 1.35
         System.out.println("Enter a number: ");
         Scanner console = new Scanner(System.in);
         double value = console.nextDouble();
         if (value < 0){
             throw new IllegalArgumentException("Please enter a value greater than or equal to 0.");
         }
+        DecimalFormat dec = new DecimalFormat("##.00");
         double cad = value * 1.35;
-        System.out.println(value + " USD is worth " + cad + " CAD.");
+        System.out.println(value + " USD is worth " + dec.format(cad) + " CAD.");
+        System.out.println();
+    }
+
+    public static void choose(char x, String y){
+        System.out.println("Type (" + x + ") to convert to " + y);
     }
 }
