dc19e500918ba93d65eb2f4eb290098cadf66129
Jenna <104884851+jennah1016@users.noreply.github.com>
2023-12-22 23:57:38 -0600

    Add files via upload

---
+++ GuessTheNumber.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class GuessTheNumber {
+    /*this program generates a random number, and asks the user to guess which number the program generated
+    this project helped me practice the following:
+        - while loops
+        - random numbers
+        - user input
+     */
+
+    public static void main (String[] args){
+        game(1, 10);
+        System.out.println("want to play again?\npress (X) to continue\npress (Y) to continue");
+        Scanner console = new Scanner(System.in);
+        String choice = console.next().toLowerCase();
+        if (choice.equals("x")){
+            game(1, 15);
+        } else if (choice.equals("y")){
+            System.out.println("thanks for playing!");
+        }
+        //game(1, 25);
+    }
+
+     public static void game (int min, int max){
+        //keeps track of how many guesses the user makes
+        int tries = 0;
+
+        //generate a humber for the user to guess
+        int rightGuess = min + (int)(max * Math.random());
+
+        //ask user to guess which the generated number
+        System.out.println("I'm thinking of a number between " + min + " and " + max + ". Guess which number it is.");
+        System.out.println("Your guess: ");
+
+        //user guesses a number
+        Scanner console = new Scanner(System.in);
+        int userGuess = console.nextInt();
+
+        //runs until the user gets right answer
+        while(userGuess != rightGuess){
+            //user's guess was lower than the generated number
+            if (userGuess < rightGuess){
+                //update number of guesses the user has made
+                tries++;
+                //ask user to guess again
+                System.out.println("Guess higher.\nYour guess: ");
+                userGuess = console.nextInt();
+            } else if (userGuess > rightGuess){
+                //same function as when the user guesses lower
+                tries++;
+                System.out.println("Guess lower.\n Your guess: ");
+                userGuess = console.nextInt();
+            }
+
+            if (userGuess == rightGuess){
+                tries++;
+                System.out.println("Correct! Your got the right answer after " + tries + " tries.");
+            }
+        }
+    }
+}
